4e7d8d0854c0b98350dfc379f75edad6d3c1cfd2
Sean McIntyre <boxysean@gmail.com>
2019-10-06 23:38:34 -0400

    Record submissions

--- api/src/main/scala/gg/kenken/aws/Handler.scala
+++ api/src/main/scala/gg/kenken/aws/Handler.scala
@@ -3,10 +3,14 @@ package gg.kenken.aws
 import scala.jdk.CollectionConverters._
 import scala.beans.BeanProperty
 import scala.collection.immutable.Map
-// import scala.collection.JavaConverters._
+import java.text
 import java.util
+import java.util.UUID.randomUUID
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import spray.json._
 import spray.json.DefaultJsonProtocol._
 
@@ -20,6 +24,30 @@ case class ApiGatewayResponse(
 )
 
 class ApiGatewayHandler extends RequestHandler[util.Map[String, Any], ApiGatewayResponse] {
+  def recordSubmission(boardInput: String, constraintString: String, boardOutput: String, error: String): Unit = {
+    var record = new java.util.HashMap[String, AttributeValue]
+
+    var tz = java.util.TimeZone.getTimeZone("UTC")
+    var df = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'")
+    df.setTimeZone(tz)
+
+    record.put("SubmissionId", new AttributeValue(randomUUID().toString))
+    record.put("SubmittedAt", new AttributeValue(df.format(new java.util.Date())))
+    record.put("BoardInput", new AttributeValue(boardInput))
+    record.put("ConstraintString", new AttributeValue(constraintString))
+
+    if (boardOutput != null) {
+      record.put("BoardOutput", new AttributeValue(boardOutput))
+    }
+
+    if (error != null) {
+      record.put("Error", new AttributeValue(error))
+    }
+
+    var ddb = AmazonDynamoDBClientBuilder.defaultClient()
+    ddb.putItem("KenkenSubmissions", record)
+  }
+
   def handleRequest(input: util.Map[String, Any], context: Context): ApiGatewayResponse = {
     val bodyJson = input.get("body").asInstanceOf[String].parseJson
 
@@ -38,31 +66,38 @@ class ApiGatewayHandler extends RequestHandler[util.Map[String, Any], ApiGateway
       "Access-Control-Allow-Credentials" -> "true".asInstanceOf[Object],
     ).asJava;
 
-    try {
-      var board = KenKenSolver.solveFromAPI(boardStrings.iterator, constraintString)
+    val boardInput = boardStrings.mkString("\n")
 
-      ApiGatewayResponse(
+    try {
+      var boardOutput = KenKenSolver.solveFromAPI(boardStrings.iterator, constraintString)
+
+      this.recordSubmission(boardInput, constraintString, boardOutput, null)
+
+      return ApiGatewayResponse(
         200,
         Map(
           "constraintString" -> constraintString,
-          "boardInput" -> boardStrings.mkString("\n"),
-          "boardOutput" -> board,
+          "boardInput" -> boardInput,
+          "boardOutput" -> boardOutput,
         ).toJson.toString,
         corsHeaders,
         true
       )
     } catch {
-      case e: Throwable =>
-        ApiGatewayResponse(
+      case e: Throwable => {
+        this.recordSubmission(boardInput, constraintString, null, e.toString)
+
+        return ApiGatewayResponse(
           500,
           Map(
             "error" -> e.toString,
             "constraintString" -> constraintString,
-            "boardInput" -> boardStrings.mkString("\n"),
+            "boardInput" -> boardInput,
           ).toJson.toString,
           corsHeaders,
           true
         )
+      }
     }
   }
 }
